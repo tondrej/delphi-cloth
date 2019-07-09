c6caf7ab49141a9ee48a489d1af95b91b362767a
Ondrej Kelle <tondrej@ondrejkelle.net>
2019-07-09 19:22:07 +0200

    Add workaround for missing import in VirtualBox guest additions

--- src/ClothDemo.dpr
+++ src/ClothDemo.dpr
@@ -14,6 +14,15 @@ uses
 
 {$R *.res}
 
+(*
+workaround for missing import in VirtualBox guest additions
+https://www.virtualbox.org/ticket/18324
+causing linker error /usr/lib/x86_64-linux-gnu/VBoxOGLcrutil.so: undefined reference to `crypt_r'
+*)
+{$ifdef LINUX}
+{$linklib 'libcrypt.so'}
+{$endif}
+
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
